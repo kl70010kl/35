297dfa5a23d461c94112033768c254a8d314198b
reiiti <reiitininomae@gmail.com>
2019-06-25 10:23:15 +0900

    Playerの基本的な移動用スクリプト追加

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float jumpForce = 10;
+    private bool isJump;
+    private Rigidbody2D rdPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isJump = true;
+        rdPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJump)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rdPlayer.AddForce(Vector2.up * jumpForce,
+                    ForceMode2D.Force);
+                print(1);
+            }
+        }
+
+
+        float h = Input.GetAxis("Horizontal");
+        rdPlayer.velocity = new Vector2(speed * h,
+        rdPlayer.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stage"))
+        {
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stage"))
+        {
+            isJump = false;
+        }
+    }
+}
