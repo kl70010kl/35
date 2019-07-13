a8f7ecd84d1555be9c343750b92826d8b99b3975
kl70010kl <45251851+kl70010kl@users.noreply.github.com>
2019-07-13 10:15:21 +0900

    HPバーを追加

---
+++ Assets/HpBarCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarCtrl : MonoBehaviour
+{
+    Slider _slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+    float _hp = 0;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hp += 1.0f;
+        if(_hp>100)
+        {
+            _hp = 0;
+        }
+        _slider.value = _hp;
+            
+    }
+}
